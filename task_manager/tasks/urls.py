3b1cd50570033d80c4b33a03bc1db1b27f406e17
AndrsBatista <andrs.batistav02@gmail.com>
2025-04-10 15:49:11 -0400

    Tercer update

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import TaskListCreate, TaskViewSet, task_list
+from .views import TaskListCreate, TaskViewSet, task_list, create_task
+from . import views
 
 router = routers.DefaultRouter()
 router.register('api/app', TaskViewSet, 'tasks')
@@ -11,6 +12,12 @@ urlpatterns = [
     
     # Ruta para la vista basada en templates
     path('', task_list, name='task_list'),
+    path('task/new/', create_task, name='create_task'),
+    path('tasks/<int:task_id>/delete/', views.delete_task, name='delete_task'),
+    path('tasks/<int:task_id>/edit/', views.edit_task, name='edit_task'),
+
+
+
 ]
 
 
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, viewsets, permissions
 from .models import Task
 from .serializers import TaskSerializer
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.cache import cache_page
 from django.core.cache import cache  # ✅ SOLO este import
 
@@ -14,6 +14,7 @@ def test_cache_view(request):
     cache.set('my_key', 'my_value', timeout=None)
     value = cache.get('my_key')
     return JsonResponse({"cached_value": value})
+
 
 class TaskListCreate(generics.ListCreateAPIView):
     queryset = Task.objects.all()
@@ -27,6 +28,37 @@ class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = TaskSerializer
+    
+
+@cache_page(60)
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+
+    if request.method == 'POST':
+        task.title = request.POST.get('title')
+        task.description = request.POST.get('description')
+        task.completed = True if request.POST.get('completed') == 'on' else False
+        task.save()
+        return redirect('home')
+
+    return render(request, 'tasks/task_edit.html', {'task': task})
+
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('home')
+
+
+@cache_page(60)
+def create_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        Task.objects.create(title=title, description=description)
+        return redirect('home')
+    
+    return render(request, 'tasks/task_create.html')
 
 @cache_page(60)  # Cache por 60 segundos
 def task_list(request):
